3a63baf923f49f2a87219383c836f58090f76826
Roman Sutormin <rsutormin@lbl.gov>
2013-11-07 18:03:52 -0800

    Internal commit (storage page is ready, module page is half ready).

--- src/widgets/spec/kbaseSpecModuleCard.js
+++ src/widgets/spec/kbaseSpecModuleCard.js
@@ -6,26 +6,79 @@
 
         options: {
             id: null,
-            width: 200
+            width: 560
         },
 
         init: function(options) {
             this._super(options);
             var self = this;
-            var $helloDiv = $("<div/>")
-                            .append("Module")
-                            .on("click", function(event) {
-                                self.trigger("showSpecElement", 
-                                    { 
-                                	  kind: "type",
-                                      id: "TestType", 
-                                      event: event
-                                    }
-                                )
-                            });
+            var container = this.$elem;
+            self.$elem.append('<p class="muted loader-table"><img src="assets/img/ajax-loader.gif"> loading...</p>');
 
-            this.$elem.append($helloDiv);
+            var kbws = new Workspace('http://localhost:9999/');
+            var wsAJAX = kbws.get_module_info(
+            		{
+            			mod: this.options.id,
+            		});
+
+            $.when(wsAJAX).done(function(data){
+
+            var tabNames = ['Types', 'Functions', 'Included modules'];
+            var tabIds = ['types', 'funcs', 'includes'];
+
+            // build tabs
+            var tabs = $('<ul id="table-tabs" class="nav nav-tabs"> \
+                           <li class="active" > \
+                            <a href="#'+tabIds[0]+'" data-toggle="tab" >'+tabNames[0]+'</a> \
+                           </li> \
+                          </ul>');
+            for (var i=1; i<tabIds.length; i++) {
+                tabs.append('<li><a href="#'+tabIds[i]+'" data-toggle="tab">'+tabNames[i]+'</a></li>');
+            }
+
+            // add tabs
+            container.append(tabs);
+            
+            var tab_pane = $('<div id="tab-content" class="tab-content">');
+            // add table views (don't hide first one)
+            tab_pane.append('<div class="tab-pane in active" id="'+tabIds[0]+'"> \
+                                <table cellpadding="0" cellspacing="0" border="0" id="'+tabIds[0]+'-table" \
+                                class="table table-bordered table-striped" style="width: 100%;"></table>\
+                            </div>');
+
+            for (var i=1; i<tabIds.length; i++) {
+                var tableDiv = $('<div class="tab-pane in" id="'+tabIds[i]+'"> ');
+                var table = $('<table cellpadding="0" cellspacing="0" border="0" id="'+tabIds[i]+'-table" \
+                                class="table table-striped table-bordered">');
+                tableDiv.append(table);
+                tab_pane.append(tableDiv);
+            }
+
+            container.append(tab_pane);
+            
+            // event for showing tabs
+            $('#table-tabs a').click(function (e) {
+                e.preventDefault();
+                $(this).tab('show');
+            });
+            
+            //$('#types-table');
+            container.append("" + data.types);
+            $('.loader-table').remove();
+
+            });
+
+            
             return this;
+        },
+        
+        getData: function() {
+            return {
+                type: "KBaseSpecModuleCard",
+                id: this.options.id,
+                workspace: '',
+                title: "Spec-document Module"
+            };
         }
     });
 })( jQuery );
--- src/widgets/spec/kbaseSpecStorageCard.js
+++ src/widgets/spec/kbaseSpecStorageCard.js
@@ -5,27 +5,64 @@
         version: "1.0.0",
 
         options: {
-            id: null,
-            width: 200
+            id: '',
+            width: 560
         },
 
         init: function(options) {
+        	options.id = '';
             this._super(options);
             var self = this;
-            var $helloDiv = $("<div/>")
-                            .append("Storage")
-                            .on("click", function(event) {
-                                self.trigger("showSpecElement", 
-                                    { 
-                                	  kind: "module",
-                                      id: "TestModule", 
-                                      event: event
-                                    }
-                                )
-                            });
+            self.$elem.append('<p class="muted loader-table"><img src="assets/img/ajax-loader.gif"> loading...</p>');
 
-            this.$elem.append($helloDiv);
+            var kbws = new Workspace('http://localhost:9999/');
+            var wsAJAX = kbws.list_modules({});
+
+            $.when(wsAJAX).done(function(data){
+            	var dataList = [];
+            	for (var i = 0; i < data.length; i++)
+            		dataList[i] = {module: '<a class="module-click" data-module="'+data[i]+'">'+data[i]+'</a>'};
+                self.$elem.append('<table id="module-table" class="table table-striped table-bordered"></table>');
+                var tableSettings = {
+                        "fnDrawCallback": modelEvents,
+                        "sPaginationType": "full_numbers",
+                        "iDisplayLength": 10,
+                        "aoColumns": [{sTitle: "Module name", mData: "module"}],
+                        "aaData": [],
+                        "oLanguage": {
+                            "sSearch": "Search module:",
+                            "sEmptyTable": "No modules registered."
+                        }
+                    };
+                var table = $('#module-table').dataTable(tableSettings);
+                table.fnAddData(dataList);
+
+                $('.loader-table').remove();
+            });
+            
+            function modelEvents() {
+                $('.module-click').unbind('click');
+                $('.module-click').click(function() {
+                    var module = $(this).data('module');
+                    self.trigger('showSpecElement', 
+                    		{
+                    			kind: "module", 
+                    			id : module,
+                    			event: event
+                    		});
+                });
+            }
+
             return this;
+        },
+        
+        getData: function() {
+            return {
+                type: "KBaseSpecStorageCard",
+                id: this.options.id,
+                workspace: '',
+                title: "Spec-document Storage"
+            };
         }
     });
 })( jQuery );
