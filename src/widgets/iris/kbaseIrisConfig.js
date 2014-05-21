cb8d1e6f9eb00caec06ff623caa3e50fe6117b9f
Jim Thomason <thomason@cshl.edu>
2014-05-21 14:37:42 -0500

    added tpl_or_def method to allow makefile variables

--- src/widgets/iris/kbaseIrisConfig.js
+++ src/widgets/iris/kbaseIrisConfig.js
@@ -6,15 +6,25 @@
 define('kbaseIrisConfig',
     function() {
         if (window.kbaseIrisConfig == undefined) {
+
+            var tpl_or_def = function (tpl, def) {
+                if (tpl.match(/\[%.+%\]/) || tpl == undefined || tpl.length == 0) {
+                    return def;
+                }
+                else {
+                    return tpl;
+                }
+            };
+
             window.kbaseIrisConfig = {
                 terminal : {
-                    invocationURL   : 'http://kbase.us/services/invocation',
-                    searchURL       : 'https://kbase.us/services/search-api/search/$category/$keyword?start=$start&count=$count&format=json',
+                    invocationURL   : tpl_or_def('[% invocationURL %]', 'http://kbase.us/services/invocation'),
+                    searchURL       : tpl_or_def('[% searchURL %]', 'https://kbase.us/services/search-api/search/$category/$keyword?start=$start&count=$count&format=json'),
                     run_dispatch    : [],
                 },
                 tutorial : {
-                    config_url       : 'http://kbase.us/docs/tutorials.cfg',
-                    default_tutorial : 'tutorial-test.html',
+                    config_url       : tpl_or_def('[% config_url %]', 'http://kbase.us/docs/tutorials.cfg'),
+                    default_tutorial : tpl_or_def('[% default_tutorial %]', 'http://kbase.us/docs/getstarted/getstarted_iris/getstarted_iris.html'),
                 },
                 commands : {
                     order   : [],
