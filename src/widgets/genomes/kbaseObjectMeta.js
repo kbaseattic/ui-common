ed59353bfd5815935852921838c8c22f225bd4da
mj <marcin@MacBookPro-20C9D0455F3D-3.local>
2014-07-11 21:14:52 -0700

    added object mappings

--- src/widgets/genomes/kbaseObjectMeta.js
+++ src/widgets/genomes/kbaseObjectMeta.js
@@ -9,12 +9,16 @@
 		version: "1.0.0",
 
 		options: {
-			workspaceID: null,
-			objectId: null,
+			objNameOrId: null,
+			wsNameOrId: null,
 			loadingImage: "assets/img/loading.gif",
-			kbCache:{}
+			kbCache:{},
+			objVer: null,
 			
 		},
+		
+		objName:"",
+		wsName:"",
 		
 		workspaceURL: "https://kbase.us/services/workspace",
 
@@ -27,17 +31,22 @@
 			this._super(options);
 			var self = this;
 			
+			
+			console.log(this.options.objNameOrId+"   "+this.options.wsNameOrId);
+			
+			/*
 			console.log("here!");
-			if (this.options.objectID === null) {
+			if (this.options.objNameOrId === null) {
 				this.renderError({ error: "No object id: " + 
-                                              this.options.workspaceID + "/" + 
-                                              this.options.objectId });
+                                              this.options.objNameOrId + "/" + 
+                                              this.options.wsNameOrId });
 				return this;
 			}
 			
 			if (options.wsUrl) {
 			self.wsUrl = options.wsUrl;
 			}
+			*/
 			
 			var kbws;
 			
@@ -53,7 +62,7 @@
 			self.objName = options.objNameOrId;
 			self.wsName = options.wsNameOrId;
 			
-			self.$elem.append('<div id="loading-mssg"><p class="muted loader-table"><center><img src="assets/img/ajax-loader.gif"><br><br>finding all data that references this object...</center></p></div>');
+			self.$elem.append('<div id="loading-mssg"><p class="muted loader-table"><center><img src="assets/img/ajax-loader.gif"><br><br>Finding metadata for this object...</center></p></div>');
 			self.$elem.append('<div id="mainview">')
             
 	    
@@ -67,8 +76,66 @@
 			this.$elem.append(this.$infoPanel);
 			
 			  var objectIdentity = self.getObjectIdentity(options.wsNameOrId, options.objNameOrId, options.objVer);
-			 kbws.list_referencing_objects([objectIdentity], function(data) {
+			 kbws.get_objects([objectIdentity], function(data) {
 				
+		console.log(data[0].length);
+		console.log(data.length);
+		if (data[0].length == 0) {
+                        self.$elem.append("<br><b>There no workspaces or objects with this id.</b>");
+                    } else {
+                        var refList = {};
+			
+                        for(var i = 0; i < data[0].length; i++) {
+                            var objInfo = data[0][i];
+                            //0:obj_id objid, 1:obj_name name, 2:type_string type,3:timestamp save_date, 4:int version, 5:username saved_by,
+                            //6:ws_id wsid, 7:ws_name workspace, 8:string chsum, 9:int size, 10:usermeta meta>
+                            //object_info;
+                            if (!(objInfo[6]+"/"+objInfo[1] in refList)) {
+                                refList[objInfo[6]+"/"+objInfo[1]] = objInfo;
+                                
+                            } else {
+                                if (refList[objInfo[6]+"/"+objInfo[1]][4] < objInfo[4]) {
+                                    refList[objInfo[6]+"/"+objInfo[1]] = objInfo;
+                                    
+                            }
+                        }
+                        
+                        // load the data into a data object
+                        var refTableData = [];
+                        for(var ref in refList) {
+                            var objInfo = refList[ref];
+                            var savedate = new Date(objInfo[3]);
+                                    refTableData.push({
+                                            na:objInfo[1]+ " ("+objInfo[6]+"/"+objInfo[0]+"/"+objInfo[4]+")",
+                                            ty:objInfo[2],
+                                            de:"saved by "+objInfo[5]+" on "+self.monthLookup[savedate.getMonth()]+" "+savedate.getDate()+", "+savedate.getFullYear()
+                                        })
+                                }
+                            //self.$elem.append("<b>"+objInfo[1]+"</b> <i>"+objInfo[2]+"</i> ("+objInfo[6]+"/"+objInfo[0]+"/"+objInfo[4]+", saved by "
+                            //            +objInfo[5]+" on "+self.monthLookup[savedate.getMonth()]+" "+savedate.getDate()+", "+savedate.getFullYear()+")<br>")
+                        }
+                        
+                        self.$elem.find('#loading-mssg').remove();
+                        
+                        var $maindiv = self.$elem.find('#mainview');
+                        $maindiv.append('<table cellpadding="0" cellspacing="0" border="0" id="ref-table" \
+                            class="table table-bordered table-striped" style="width: 100%; margin-left: 0px; margin-right: 0px;"/>');
+
+            		var tblSettings = {
+            				"sPaginationType": "full_numbers",
+            				"iDisplayLength": 10,
+                                        "sDom": 't<flip>',
+            				"aoColumns": [
+            				              {sTitle: "Object Name (reference)", mData: "na", sWidth:"30%"},
+            				              {sTitle: "Type", mData: "ty"},
+            				              {sTitle: "Details", mData: "de"}
+            				              ],
+            				              "aaData": refTableData            				              
+            		};
+            		var refTable = self.$elem.find('#ref-table').dataTable(tblSettings);
+                    }
+		    
+	/*
 			 $.when(wsobject).fail($.proxy(function(error) {
                 this.renderError(error);
                 console.log(error);
@@ -104,6 +171,7 @@
                 this.hideMessage();
                 this.$infoPanel.show();
             }, this));
+	    */
         	
 			
 				
@@ -116,6 +184,11 @@
 	return this;
         },
 		
+		
+		  getData: function() {
+            return {title:"Metadata for :",id:this.objName, workspace:this.wsName};
+        },
+        
 		/**
 		 *Returns the full workspace identified, even with version.
 		 */
