f44d9bfda30d6ffb7c0a0b01ed53d6bc7fd25042
mj <marcin@MacBookPro-20C9D0455F3D-3.local>
2014-07-15 15:40:29 -0700

    working version

--- src/widgets/genomes/kbaseObjectMeta.js
+++ src/widgets/genomes/kbaseObjectMeta.js
@@ -8,20 +8,20 @@
 		parent: "kbaseWidget",
 		version: "1.0.0",
 
+		wsUrl:"https://kbase.us/services/ws",
+
 		options: {
 			objNameOrId: null,
 			wsNameOrId: null,
-			loadingImage: "assets/img/loading.gif",
-			kbCache:{},
 			objVer: null,
-			
+			loadingImage: "assets/img/loading.gif",
+			kbCache:{},			
+			width:900			
 		},
 		
 		objName:"",
 		wsName:"",
 		
-		workspaceURL: "https://kbase.us/services/workspace",
-
 		//cdmiURL: "https://kbase.us/services/cdmi_api",
 
 		/**
@@ -29,25 +29,13 @@
 		 */
 		init: function(options) {
 			this._super(options);
-			var self = this;
-			
+			var self = this;			
 			
 			console.log(this.options.objNameOrId+"   "+this.options.wsNameOrId);
-			
-			/*
-			console.log("here!");
-			if (this.options.objNameOrId === null) {
-				this.renderError({ error: "No object id: " + 
-                                              this.options.objNameOrId + "/" + 
-                                              this.options.wsNameOrId });
-				return this;
-			}
-			
+						
 			if (options.wsUrl) {
-			self.wsUrl = options.wsUrl;
+			    self.wsUrl = options.wsUrl;
 			}
-			*/
-			
 			var kbws;
 			
 			if (self.options.kbCache.ws_url) {
@@ -64,9 +52,8 @@
 			
 			self.$elem.append('<div id="loading-mssg"><p class="muted loader-table"><center><img src="assets/img/ajax-loader.gif"><br><br>Finding metadata for this object...</center></p></div>');
 			self.$elem.append('<div id="mainview">')
-            
-	    
-			
+            	    
+			/*
 			 this.$infoPanel = $("<div>");
 			 this.$infoTable = $("<table>")
                               .addClass("table table-striped table-bordered");
@@ -74,128 +61,88 @@
                            .append(this.$buttonPanel);
 
 			this.$elem.append(this.$infoPanel);
+			*/
+			
+			console.log("objVer "+options.objVer);
 			
 			  var objectIdentity = self.getObjectIdentity(options.wsNameOrId, options.objNameOrId, options.objVer);
-			 kbws.get_objects([objectIdentity], function(data) {
+			  console.log(objectIdentity);
+			  console.log([objectIdentity]);
+			  
+			  var pass = {workspace:options.wsNameOrId,name:options.objNameOrId};
+			  console.log(pass);
+			 //kbws.get_objects([objectIdentity], function(data) {
+			 //kbws.get_objects([{workspace: this.options.wsNameOrId, name: this.options.objNameOrId}], function(data) {
+			//kbws.list_referencing_objects([pass], function(data) {								
+			kbws.get_object_info([objectIdentity]).fail(function (xhr, status, error) {
+                            console.log(xhr);
+                            console.log(status);
+                            console.log(error);
+                        })
+                        .done(function (info, status, xhr) {
+                            setTimeout(function() { ; }, 200);                        
+                            //console.log(info);
+			    console.log(info[0]);
+			    var objinfo = info[0];
+                            var refList = {};													
+				// load the data into a data object
 				
-		console.log(data[0].length);
-		console.log(data.length);
-		if (data[0].length == 0) {
-                        self.$elem.append("<br><b>There no workspaces or objects with this id.</b>");
-                    } else {
-                        var refList = {};
-			
-                        for(var i = 0; i < data[0].length; i++) {
-                            var objInfo = data[0][i];
-                            //0:obj_id objid, 1:obj_name name, 2:type_string type,3:timestamp save_date, 4:int version, 5:username saved_by,
-                            //6:ws_id wsid, 7:ws_name workspace, 8:string chsum, 9:int size, 10:usermeta meta>
-                            //object_info;
-                            if (!(objInfo[6]+"/"+objInfo[1] in refList)) {
-                                refList[objInfo[6]+"/"+objInfo[1]] = objInfo;
-                                
-                            } else {
-                                if (refList[objInfo[6]+"/"+objInfo[1]][4] < objInfo[4]) {
-                                    refList[objInfo[6]+"/"+objInfo[1]] = objInfo;
-                                    
-                            }
-                        }
-                        
-                        // load the data into a data object
-                        var refTableData = [];
-                        for(var ref in refList) {
-                            var objInfo = refList[ref];
-                            var savedate = new Date(objInfo[3]);
-                                    refTableData.push({
-                                            na:objInfo[1]+ " ("+objInfo[6]+"/"+objInfo[0]+"/"+objInfo[4]+")",
-                                            ty:objInfo[2],
-                                            de:"saved by "+objInfo[5]+" on "+self.monthLookup[savedate.getMonth()]+" "+savedate.getDate()+", "+savedate.getFullYear()
-                                        })
-                                }
-                            //self.$elem.append("<b>"+objInfo[1]+"</b> <i>"+objInfo[2]+"</i> ("+objInfo[6]+"/"+objInfo[0]+"/"+objInfo[4]+", saved by "
-                            //            +objInfo[5]+" on "+self.monthLookup[savedate.getMonth()]+" "+savedate.getDate()+", "+savedate.getFullYear()+")<br>")
-                        }
-                        
-                        self.$elem.find('#loading-mssg').remove();
-                        
-                        var $maindiv = self.$elem.find('#mainview');
-                        $maindiv.append('<table cellpadding="0" cellspacing="0" border="0" id="ref-table" \
-                            class="table table-bordered table-striped" style="width: 100%; margin-left: 0px; margin-right: 0px;"/>');
-
-            		var tblSettings = {
-            				"sPaginationType": "full_numbers",
-            				"iDisplayLength": 10,
-                                        "sDom": 't<flip>',
-            				"aoColumns": [
-            				              {sTitle: "Object Name (reference)", mData: "na", sWidth:"30%"},
-            				              {sTitle: "Type", mData: "ty"},
-            				              {sTitle: "Details", mData: "de"}
-            				              ],
-            				              "aaData": refTableData            				              
-            		};
-            		var refTable = self.$elem.find('#ref-table').dataTable(tblSettings);
-                    }
-		    
-	/*
-			 $.when(wsobject).fail($.proxy(function(error) {
-                this.renderError(error);
-                console.log(error);
-            }, this));
-			 
-            $.when(wsobject).done($.proxy(function(wsobject) {
-                if (wsobject) {
-                
-                        this.$infoTable.empty();
-			
-			//0:obj_id objid, 1:obj_name name, 2:type_string type,3:timestamp save_date, 4:int version, 5:username saved_by,
-                            //6:ws_id wsid, 7:ws_name workspace, 8:string chsum, 9:int size, 10:usermeta meta>
-			
-			this.$infoTable.append(this.makeRow("Name", wsobject[2]));
-			this.$infoTable.append(this.makeRow("Id", wsobject[1]));
-			this.$infoTable.append(this.makeRow("Last save", wsobject[3]));
-			this.$infoTable.append(this.makeRow("Version", wsobject[4]));
-                        this.$infoTable.append(this.makeRow("Owner", wsobject[5]));
-			this.$infoTable.append(this.makeRow("Workspace Id", wsobject[6]));
-			this.$infoTable.append(this.makeRow("Workspace name", wsobject[7]));
-			this.$infoTable.append(this.makeRow("Checksum", wsobject[8]));
-			this.$infoTable.append(this.makeRow("Size", wsobject[9]));
-			this.$infoTable.append(this.makeRow("User metadata", wsobject[10]));
-			
-                    
-                }
-                else {
-                    this.renderError({ error: "No object id: " + 
-                                              this.options.wsNameOrId + "/" + 
-                                              this.options.objNameOrId });
-                }
-
-                this.hideMessage();
-                this.$infoPanel.show();
-            }, this));
-	    */
-        	
-			
+				var refTableData = [];		
+				var savedate = new Date(objinfo[3]);					    
+				refTableData.push({
+				  na:objinfo[1],
+				  ve:objinfo[6]+"/"+objinfo[0]+"/"+objinfo[4],
+				  ty:objinfo[2],
+				  ow:objinfo[5],
+				  da:self.monthLookup[savedate.getMonth()]+" "+savedate.getDate()+", "+savedate.getFullYear(),
+			          si:Math.round((objinfo[9]/1048576.0)*10.0)/10.0+" MB",
+				  ch:objinfo[8]
+				})
+											
+				self.$elem.find('#loading-mssg').remove();
 				
-        } , function(err) {
-                    self.$elem.find('#loading-mssg').remove();
-                    self.$elem.append("<br><b>No workspace or object found with these ids.</b>");
-                    console.error("No workspace or object found with these ids.");
-                    console.error(err);
-                });
+				var $maindiv = self.$elem.find('#mainview');
+				$maindiv.append('<table cellpadding="0" cellspacing="0" border="0" id="ref-table" \
+				    class="table table-bordered table-striped" style="width: 100%; margin-left: 0px; margin-right: 0px;"/>');
+	
+				var tblSettings = {
+						//"sPaginationType": "full_numbers",
+						//"iDisplayLength": 10,
+						"autoWidth": true,
+						"sDom": 't<>',
+						"ordering": false,
+						"aoColumns": [
+							      {sTitle: "Object Name", mData: "na", sWidth:"30%"},
+							      {sTitle: "WS Id/Obj Id/Version", mData: "ve"},
+							      {sTitle: "Type", mData: "ty"},
+							      {sTitle: "Owner", mData: "ow"},
+							      {sTitle: "Last modified", mData: "da"},
+							      {sTitle: "Size", mData: "si"},
+							      {sTitle: "Checksum", mData: "ch"}
+							      ],
+							      "aaData": refTableData            				              
+				};
+				var refTable = self.$elem.find('#ref-table').dataTable(tblSettings);	     
+			  });		     
+			
 	return this;
         },
 		
 		
-		  getData: function() {
+	getData: function() {
             return {title:"Metadata for :",id:this.objName, workspace:this.wsName};
         },
         
 		/**
-		 *Returns the full workspace identified, even with version.
+		 *Returns the full workspace identifier, optionally with the version.
 		 */
 		getObjectIdentity: function(wsNameOrId, objNameOrId, objVer) {
+			console.log(wsNameOrId +" "+objNameOrId +" "+ objVer);
 			if (objVer) { return {ref:wsNameOrId+"/"+objNameOrId +"/"+objVer}; }
 			return {ref:wsNameOrId+"/"+objNameOrId } ;
 		},
+		
+		monthLookup : ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep","Oct", "Nov", "Dec"]
 	
 	});
 })( jQuery );
