90c1571444572ffaed5e77bc859d59405718a2c9
Janaka Edirisinghe <janaka.e@gmail.com>
2014-07-17 16:58:47 -0500

    tabs in Simulation set

--- src/widgets/genomes/kbaseSimulationSet.js
+++ src/widgets/genomes/kbaseSimulationSet.js
@@ -18,7 +18,7 @@ init: function(options) {
             data = $.extend(data,{});
             console.log (data[0])
             //this.$elem.append(JSON.stringify(data))
-            console.log(data[0].data.id, data[0].info[7]);
+            //console.log(data[0].data.id, data[0].info[7]);
             var simu = data[0].data;
                     
 /*
@@ -34,6 +34,40 @@ init: function(options) {
 
                 this.$elem.append (table)
 */
+  
+                var container = this.$elem;
+                var simuTable = $('<table class="table table-bordered table-striped" style="width: 100%;">');
+                var tabs = container.tabs({tabs: [
+                                            {name: 'Overview', active: true},
+                                            {name: 'SimulationSet', content: simuTable}]
+                                          })
+
+                var keys = [
+                    {key: 'wsid'},
+                    {key: 'ws'},
+                    {key: 'kbid'},
+                    {key: 'source'},
+                    {key: 'type'},
+                    {key: 'errors'},
+                    {key: 'owner'},
+                    {key: 'date'}
+                ];
+
+                var simudata = {
+                    wsid: data[0].info[1],
+                    ws: data[0].info[7],
+                    kbid: simu.id,
+                    source: simu.phenoclass,
+                    type: simu.simulatedGrowth,
+                    errors: simu.simulatedGrowthFraction,
+                    owner: data[0].creator,
+                    date: data[0].created,
+                };
+
+                var labels = ['Name','Workspace','KBID','Type','Errors','Owner','Creation date'];
+                var table = kb.ui.objTable('overview-table',simudata,keys,labels);
+                tabs.tabContent('Overview').append(table)
+
                 var tableSettings = {
 					 "sPaginationType": "bootstrap",
 					 "iDisplayLength": 10,
@@ -54,9 +88,10 @@ init: function(options) {
 					 
 				}
 
-				var simu_table = $('<table class="table table-striped table-bordered">')
-				this.$elem.append(simu_table)
-				simu_table.dataTable(tableSettings);
+
+				simuTable.dataTable(tableSettings);
+
+                       
 
             return this;
             
