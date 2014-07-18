6e48b2e0a1131f5ca5a2bd5a21bf4f7f949c6e0b
Paramvir Dehal <psdehal@lbl.gov>
2014-07-18 13:49:40 -0700

    Bar graph appears on screen, still having some scoping problem causing subcategories to not be visible`

--- src/widgets/genomes/kbaseSEEDFunctions.js
+++ src/widgets/genomes/kbaseSEEDFunctions.js
@@ -24,8 +24,20 @@
             width:900         
         },
         
-        SEEDTree:{ "count": 0, "children":[] },
+        SEEDTree:{ "name":"SEED", "count": 0, "children":[], "size":0, "x0":0, "y0":0 },
         subsysToGeneMap:[],
+
+        margin:{top: 30, right: 20, bottom: 30, left: 20},
+        width: 920,
+        barHeight : 20,
+        barWidth:400,
+        svg:null,
+
+        i : 0,
+        duration : 400,
+        root:null,
+
+        tree:null,
 
         objName:"",
         wsName:"",
@@ -37,6 +49,7 @@
 
         init: function(options) {
             this._super(options);          
+            this.render();
 
             var SEEDTree = this.SEEDTree;
             var subsysToGeneMap = this.subsysToGeneMap;
@@ -77,12 +90,11 @@
                 this.loadSEEDHierarchy();
 
             }, this));
-
-            this.render();
             
 
             return this;
         },
+
 
         /*
           I need to load the SEED subsystem ontology. I am going to use
@@ -105,12 +117,12 @@
           loadSEEDHierarchy() function will parse file and populate the SEEDTree data structure
         */
         loadSEEDHierarchy: function() {
-
+            var self = this;
             var ontologyDepth = 4; //this should be moved up to the global variables
             var nodeMap = {};
 
-            var SEEDTree = this.SEEDTree;
-            var subsysToGeneMap = this.subsysToGeneMap;
+            var SEEDTree = self.SEEDTree;
+            var subsysToGeneMap = self.subsysToGeneMap;
 
             d3.text("assets/data/subsys.txt", function(text) {
                 var data = d3.tsv.parseRows(text);
@@ -162,15 +174,132 @@
                     }
                 }
                 
-            });
-
-            SEEDTree.children.forEach(this.collapse);
+            console.log("something " + self.SEEDTree.children.length);
+            $.when( 
+                self.SEEDTree.children.forEach(function(d) {
+                    self.collapse(d)}) 
+                ).done(
+                    self.update(self.SEEDTree)
+                );
+            //this.update(this.root = SEEDTree);
+            }); //).done( $.when( SEEDTree.children.forEach(self.collapse) ).done( self.update(self.SEEDTree)) );
+            //this.update(this.root = SEEDTree);
         },
 
-        collapse: function() {
+
+        update: function(source) {
+
+            //if (this.tree === null) { this.tree = d3.layout.tree().nodeSize([0, 4]);}
+            var self = this;
+            var tree = d3.layout.tree().nodeSize([0, 4]);
+            var nodes = tree.nodes(self.SEEDTree);
+            console.log("Z: " + this.SEEDTree.children.length);
+            var scale = d3.scale.linear().domain([0,400]).range([0,290]);
+            var height = Math.max(500, nodes.length * this.barHeight + this.margin.top + this.margin.bottom);
+            var i = self.i;
+            d3.select("svg").transition()
+                .duration(this.duration)
+                .attr("height", height);
+
+            d3.select(self.frameElement).transition()
+                .duration(this.duration)
+                .style("height", height + "px");
+
+            // Compute the "layout".
+            nodes.forEach(function(n, i) {
+                n.x = i * self.barHeight;
+            });
+
+            // Update the nodes…
+            var node = this.svg.selectAll("g.node")
+                .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+            var nodeEnter = node.enter().append("g")
+                .attr("class", "node")
+                .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+                .style("opacity", 1e-6);
+
+            // Enter any new nodes at the parent's previous position.
+            nodeEnter.append("rect")
+                .attr("y", -this.barHeight / 2)
+                .attr("x", 300)
+                .attr("height", self.barHeight)
+                .attr("width", self.barWidth)
+                .style("fill", self.color)
+                .on("click", $.proxy(function(d) {self.click(d)}, self));
+
+            nodeEnter.append("text")
+                .attr("dy", 3.5)
+                .attr("dx", 300 + 5.5)
+                .text(function(d) { return d.name; });
+
+            nodeEnter.append("rect")
+                .attr("y", -self.barHeight / 2)
+                //.attr("x", function (d) { return 0 - d.depth * 4;} )
+                .attr("x", function (d) { return 0 + 295 - scale(d.size) - d.depth * 4;} )
+                .attr("height", self.barHeight)
+                .attr("width", function (d) { return scale(d.size); })
+                .style("fill", self.color);
+
+            nodeEnter.append("text")
+                .attr("dy", 3.5)
+                .attr("dx", 5.5)
+                .text(function(d) { return d.size; });
+
+            // Transition nodes to their new position.
+            nodeEnter.transition()
+                .duration(this.duration)
+                .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+                .style("opacity", 1);
+
+            node.transition()
+                .duration(this.duration)
+                .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+                .style("opacity", 1)
+                .select("rect")
+                .style("fill", self.color);
+
+            // Transition exiting nodes to the parent's new position.
+            node.exit().transition()
+                .duration(this.duration)
+                .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+                .style("opacity", 1e-6)
+                .remove();
+
+
+            // Stash the old positions for transition.
+            nodes.forEach(function(d) {
+                d.x0 = d.x;
+                d.y0 = d.y;
+            });
+
+        },
+
+        // Toggle children on click.
+        click: function(d) {
             if (d.children) {
                 d._children = d.children;
-                d._children.forEach(collapse);
+                d.children = null;
+            } else {
+                d.children = d._children;
+                d._children = null;
+            }
+            console.log(this);
+            this.update(d);
+        },
+
+        color: function(d) {
+            return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+        },
+
+        collapse: function(d) {
+            var self = this;
+            if (d.children) {
+                d._children = d.children;
+                //for(var i = 0; i < d._children.length; i++) {
+                //    self.collapse(d._children[i]);
+                //}
+                d._children.forEach( function(n) {self.collapse(n)});
                 d.children = null;
             }
         },
@@ -180,15 +309,17 @@
         },
 
         render: function() {
-            var margin = {top: 30, right: 20, bottom: 30, left: 20},
-                width = 960 - margin.left - margin.right;
+            var margin =  this.margin,
+                width = this.width;
+                //svg = this.svg;
+
 
             this.$elem.append('<div id="mainview">');
 
-            var svg = d3.select("#mainview").append("svg")
-                        .attr("width", width + margin.left + margin.right)
-                        .append("g")
-                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            this.svg = d3.select("#mainview").append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
         }
 
