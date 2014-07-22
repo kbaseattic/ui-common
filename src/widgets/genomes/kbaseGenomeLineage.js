0d65c486f287f4836ddcfcd51aa25999019fac7f
Roman Sutormin <rsutormin@lbl.gov>
2014-07-22 13:34:00 -0700

    Genome card for taxonomy lineage includes now way to search for taxonomy from closely related public genomes.

--- src/widgets/genomes/kbaseGenomeLineage.js
+++ src/widgets/genomes/kbaseGenomeLineage.js
@@ -5,7 +5,7 @@
 (function ($, undefined) {
     $.KBWidget({
         name: "KBaseGenomeLineage",
-        parent: "kbaseWidget",
+        parent: "kbaseAuthenticatedWidget",
         version: "1.0.0",
 
         options: {
@@ -20,8 +20,9 @@
 
         token: null,
         cdmiURL: "https://kbase.us/services/cdmi_api",
+        treesURL: "http://dev19.berkeley.kbase.us:7047",
         $infoTable: null,
-
+        pref: null,
 
         init: function (options) {
 
@@ -31,7 +32,8 @@
                 //throw an error
                 return;
             }
-
+            
+            this.pref = this.uuid();
 
             this.$messagePane = $("<div/>").addClass("kbwidget-message-pane").hide();
             this.$elem.append(this.$messagePane);
@@ -77,43 +79,18 @@
             // Scientific Name
             // Taxonomy
             // Taxonomy
-            this.entityClient.get_entity_Genome([this.options.genomeID], ['id', 'scientific_name', 'domain', 'taxonomy'],
-
-            $.proxy(function (genome) {
-                    genome = genome[0].data;
-                console.log(genome);
-                
-                if(genome.taxonomy.length > 0) {
-                    console.log(genome.taxonomy);
-                    var splittax = genome.taxonomy.replace("/ /g", "");
-                    splittax = splittax.split(";");
-                    var finaltax = "<pre>";
-                    for(a=0;a<splittax.length;a++) {
-                        var pad ="";
-                        for(b=0;b<a;b++){
-                            pad+=" ";
-                        }
-                        //http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?name=drosophila+miranda
-                        var searchtax = splittax[a].replace("/ /g", "+");
-                        console.log(searchtax);
-                        var str =pad+'<a href="http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?name='+searchtax+'">'+splittax[a]+'</a><br>';
-                        console.log(str);
-                       finaltax+=str; 
-                    }
-                    finaltax += "</pre>";
-                    this.$infoTable.empty().append(this.addInfoRow("ID", genome.id)).append(this.addInfoRow("Name", genome.scientific_name)).append(this.addInfoRow("Taxonomic lineage", finaltax));
-    
-                    self.hideMessage();
-                    this.$infoPanel.show();
-            }
-            else {
-                self.hideMessage();
-                this.showMessage("No taxonomic data for this genome.");
-            }
-            }, this),
-
-            this.renderError);
-
+            this.entityClient.get_entity_Genome(
+            		[this.options.genomeID], 
+            		['id', 'scientific_name', 'domain', 'taxonomy'],
+            		$.proxy(
+            				function (genome) {
+            					genome = genome[0].data;
+            					self.showData(genome);
+            				}, 
+            				this
+            		),
+            		this.renderError
+            );
         },
 
         renderWorkspace: function () {
@@ -134,44 +111,84 @@
 
             var prom = this.options.kbCache.req('ws', 'get_objects', [obj]);
             $.when(prom).done($.proxy(function (genome) {
-
                 genome = genome[0].data;
-                console.log(genome);
-                
-                if(genome.taxonomy.length > 0) {
-                console.log(genome.taxonomy);
-                var splittax = genome.taxonomy.replace("/ /g", "");
-                splittax = splittax.split(";");
-                var finaltax = "<pre>";
-                for(a=0;a<splittax.length;a++) {
-                    var pad ="";
-                    for(b=0;b<a;b++){
-                        pad+=" ";
-                    }
-                    //http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?name=drosophila+miranda
-                    var searchtax = splittax[a].replace("/ /g", "+");
-                    console.log(searchtax);
-                    var str =pad+'<a href="http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?name='+searchtax+'">'+splittax[a]+'</a><br>';
-                    console.log(str);
-                   finaltax+=str; 
-                }
-                finaltax += "</pre>";
-                this.$infoTable.empty().append(this.addInfoRow("ID", genome.id)).append(this.addInfoRow("Name", genome.scientific_name)).append(this.addInfoRow("Taxonomic lineage", finaltax));
-
-                self.hideMessage();
-                this.$infoPanel.show();
-            }
-            else {
-                self.hideMessage();
-                this.showMessage("No taxonomic data for this genome.");
-            }
+                self.showData(genome);
             }, this));
             $.when(prom).fail($.proxy(function (error) {
                 this.renderError(error);
             }, this));
         },
 
+        showData: function(genome) {
+            var self = this;
+            console.log(genome);
+            
+            this.$infoTable.empty()
+            	.append(this.addInfoRow("ID", genome.id))
+            	.append(this.addInfoRow("Name", genome.scientific_name))
+            	.append('<tr><td>Taxonomic lineage</td><td id="tax_td_'+this.pref+'"/></tr>');
 
+            self.hideMessage();
+            this.$infoPanel.show();
+            this.showLinage(genome.taxonomy);
+        },
+        
+        showLinage: function(taxonomy) {
+        	var self = this;
+            console.log("Taxonomy: " + taxonomy);
+            var finaltax = "";
+            var needGuess = this.options.workspaceID != null;
+            if (taxonomy) {
+            	var splittax = taxonomy.replace("/ /g", "");
+            	splittax = splittax.split(";");
+            	if (splittax.length >= 2)
+            		needGuess = false;
+            	finaltax += "<pre>";
+            	for(a=0;a<splittax.length;a++) {
+            		var pad ="";
+            		for(b=0;b<a;b++){
+            			pad+=" ";
+            		}
+            		//http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?name=drosophila+miranda
+            		var searchtax = splittax[a].replace("/ /g", "+");
+            		console.log(searchtax);
+            		var str =pad+'<a href="http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?name='+searchtax+'" target="_blank">'+splittax[a]+'</a><br>';
+            		console.log(str);
+            		finaltax+=str; 
+            	}
+            	finaltax += "</pre>";
+            } else {
+            	finaltax += "No taxonomic data for this genome.";
+            }
+            if (needGuess) {
+            	finaltax += '<button id="guess_btn_'+this.pref+'">Search by similarity</button>';
+            }
+			var tdElem = $('#tax_td_'+self.pref);
+			tdElem.html(finaltax);
+
+            if (needGuess) {
+            	$('#guess_btn_'+this.pref).click(function() {
+            		self.guessLinage();
+            	});
+            }
+        },
+        
+        guessLinage: function() {
+        	var self = this;
+			var tdElem = $('#tax_td_'+self.pref);
+			tdElem.html("<img src='" + this.options.loadingImage + "'>");
+        	var treesSrv = new KBaseTrees(this.treesURL, {token: this.authToken()});
+    		console.log("kbaseGenomeLineage: guess_taxonomy_path...");
+    		var genomeRef = this.options.workspaceID + "/" + this.options.genomeID;
+        	treesSrv.guess_taxonomy_path({query_genome: genomeRef}, function(data) {
+        		console.log(data);
+        		self.showLinage(data);
+        	},
+        	function(data) {
+				tdElem.html("Error accessing [trees] service: " + data.error.message);
+    		});
+        },
+        
         getData: function () {
             return {
                 title: "Taxonomic lineage for :",
@@ -225,5 +242,12 @@
             return "<tr><td>" + a + "</td><td>" + b + "</td></tr>";
         },
 
+        uuid: function() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, 
+                function(c) {
+                    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                    return v.toString(16);
+                });
+        }
     });
 })(jQuery);
