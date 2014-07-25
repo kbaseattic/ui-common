f9cf25a5612d0b51b5e9412fff06254ba008f983
msneddon <mwsneddon@lbl.gov>
2014-07-25 15:00:36 -0700

    updated CDS gene page redirect

--- functional-site/js/directives/cards.js
+++ functional-site/js/directives/cards.js
@@ -93,10 +93,16 @@ angular.module('card-directives')
             link: function(scope, element, attrs) {
                 if (cardManager) cardManager.destroy();
                 
-                if(scope.params.ws == "CDS" ) {
-                        scope.params.ws = "KBasePublicGenomesV3";
+                if(scope.params.workspaceID == "CDS" ) {
+                    scope.params.workspaceID = "KBasePublicGenomesV3";
                 }
-		
+                if (!scope.params.genomeID) {
+                    var temp = scope.params.featureID.split(".");
+                    if (temp.length>3) {
+                        scope.params.genomeID = temp[0]+"."+temp[1];
+                    }
+                }
+		alert(JSON.stringify(scope.params));
                 cardManager = $(element).KBaseCardLayoutManager({
                     template: "gene", 
                     data: scope.params, 
