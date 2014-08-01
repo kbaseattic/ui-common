3e7ae4870f812a99337a316c04395bc407cc3271
Roman Sutormin <rsutormin@lbl.gov>
2014-08-01 11:47:33 -0700

    KBASE-915 #close Sort order in gene table (genome landing page) was by contig id and then by start position. I swithed it to sorting just by feature id.

--- src/widgets/genomes/kbaseGenomeGeneTable.js
+++ src/widgets/genomes/kbaseGenomeGeneTable.js
@@ -107,15 +107,15 @@
             		var genesSettings = {
             				"sPaginationType": "full_numbers",
             				"iDisplayLength": 10,
-            				"aaSorting" : [[1,'asc'],[2,'asc']],
-					"aoColumns": [
+            				"aaSorting" : [[0,'asc']],  //[1,'asc'],[2,'asc']],
+            				"aoColumns": [
 			                              {sTitle: "Gene ID", mData: "id"}, 
             				              {sTitle: "Contig", mData: "contig"},
-			{sTitle: "Start", mData: "start", sWidth:"7%"},
-			{sTitle: "Strand", mData: "dir", sWidth:"7%"},
-			{sTitle: "Length", mData: "len", sWidth:"7%"},
-			{sTitle: "Type", mData: "type", sWidth:"10%"},
-			{sTitle: "Function", mData: "func", sWidth:"45%"}
+            				              {sTitle: "Start", mData: "start", sWidth:"7%"},
+            				              {sTitle: "Strand", mData: "dir", sWidth:"7%"},
+            				              {sTitle: "Length", mData: "len", sWidth:"7%"},
+            				              {sTitle: "Type", mData: "type", sWidth:"10%"},
+            				              {sTitle: "Function", mData: "func", sWidth:"45%"}
             				              ],
             				              "aaData": [],
             				              "oLanguage": {
