34adfe33cf6d0bf28cda3341053b17cff2ad8bbd
Erik Pearson <eapearson@lbl.gov>
2015-01-08 09:32:03 -0800

    Converted and updated popular narratives.

--- src/widgets/social/kbaseUserPopularNarratives.js
+++ src/widgets/social/kbaseUserPopularNarratives.js
@@ -1,167 +1,204 @@
-(function( $, undefined ) {
-    $.KBWidget({
-        name: "KBaseUserPopularNarratives",
-        parent: "kbaseAuthenticatedWidget",
-        version: "1.0.0",
-        
-        userNameFetchUrl:"https://kbase.us/services/genome_comparison/users?usernames=",
-	
-	
-        options: {
-            userInfo:null,
-	    wsUserInfoUrl:"https://dev04.berkeley.kbase.us:7058",
-	    wsUserInfoRef:"",
-            kbCache:{},
-        },
-    
-	wsUserName:"",
-	wsUserInfoClient:null,
-	loggedIn:false,
-	loggedInUserId:null,
-	userInfoData:null,
-	isMe:false,
-	
-	alertPanel:null,
-	
-        init: function(options) {
-            this._super(options);
-            var self = this;
-	    /* we don't need the user profile data for now...
-	     if (options.wsUserInfoUrl) {
-		if (options.kbCache.token) {
-		    self.wsUserInfoClient = new Workspace(options.wsUserInfoUrl, {token: self.options.kbCache.token});
-		} else {
-		    self.wsUserInfoClient = new Workspace(options.wsUserInfoUrl);
-		}
-            }
-	    */
-	    if (self.options.kbCache.ws_url) {
-                self.wsUrl = self.options.kbCache.ws_url;
-            }
-            if (self.options.kbCache.token) {
-                self.ws = new Workspace(self.wsUrl, {token: self.options.kbCache.token});
-		self.loggedIn = true;
-		self.loggedInUserId = $('<div></div>').kbaseLogin().get_kbase_cookie('user_id');
-            } else {
-                self.ws = new Workspace(self.wsUrl);
-            }
-	    
-	    if (options.userInfo) {
-		self.userInfoData = options.userInfo['data'];
-		self.wsUserName = self.userInfoData['basic_personal_info']['user_name'];
-		if (self.wsUserName===self.loggedInUserId) {
-		    self.isMe = true;
-		}
-	    }
-	    // setup the alert panel
-            self.alertPanel = $("<div></div>");
-	    self.$elem.append(self.alertPanel);
-            self.$mainPanel = $("<div></div>").css("overflow","auto").css("height","300px");
-	    self.$elem.append(self.$mainPanel);
-	    
-	    self.getRecentActivity();
-	    self.render();
-	    
-	    return this;
-	},
-    
-	popularNarratives:null,
-    
-	getRecentActivity : function () {
-	    var self = this;
-	    self.ws.list_workspace_info({},
-		function(data) {
-		    var wsids = [];
-		    for(var k=0; k<data.length; k++) {
-			//tuple<ws_id id, ws_name workspace, username owner, timestamp moddate,
-			//int object, permission user_permission, permission globalread,
-			//lock_status lockstat, usermeta metadata> workspace_info
-			if (data[k][2]===self.wsUserName) {
-			    //for now, only include workspaces owned by this user
-			    wsids.push(data[k][0]);
-			}
-		    }
-		    var params = {savedby:[self.wsUserName], type:"KBaseNarrative.Narrative",ids:wsids,includeMetadata:1};
-		    self.ws.list_objects(params,
-			function(data) {
-			    self.popularNarratives=[];
-			    for(var k=0; k<data.length; k++) {
-				//<obj_id objid, obj_name name, type_string type,
-				//timestamp save_date, int version, username saved_by,
-				//ws_id wsid, ws_name workspace, string chsum, int size, usermeta meta>
-				self.popularNarratives.push({
-				    name:data[k][10]['name'],
-				    ws:data[k][7],
-				    size:data[k][9],
-				    version: data[k][4],
-				    date:data[k][3]
-				});
-			    }
-				    
-			    self.popularNarratives.sort(function(a,b) {
-				//var x = new Date(a.date);
-				//var y = new Date(b.date);
-				//return ((x < y) ? 1 : ((x > y) ?  -1 : 0));
-				return ((a.version < b.version) ? 1 : ((a.version > b.version) ?  -1 : 0));
-			    });
-			    var limit = 5;
-			    if (self.popularNarratives.length>limit) {
-				self.popularNarratives = self.popularNarratives.slice(0,limit);
-			    }
-			    self.render();
-			},
-			function(err) {
-			    self.popularNarratives=[];
-			    self.render();
-			});
-		},
-		function(err) {
-		    self.popularNarratives=[];
-		    self.render();
-		});
-		
-	},
-	
-	render : function () {
-	    var self = this;
-	    
-	    self.$mainPanel.empty();
-	    
-	    // simple table view
-	    if (self.popularNarratives) {
-		var $tbl = $('<table cellpadding="0" cellspacing="0" border="0" class="table table-bordered table-striped">').css("width","90%");
-		
-		if (self.popularNarratives.length>0) {
-		    for (var k=0; k<self.popularNarratives.length; k++) {
-			var d = new Date(self.popularNarratives[k]["date"]);
-			
-			var time = "";
-			var minutes = d.getMinutes(); if (minutes<10) { minutes = "0"+minutes; }
-			if (d.getHours()>=12) {
-			    if(d.getHours()!=12) { time = (d.getHours()-12) + ":"+minutes+"pm"; }
-			    else { time = "12:"+minutes+"pm"; }
-			} else {
-			    time = d.getHours() + ":"+minutes+"am";
-			}
-			var objhtml = self.popularNarratives[k]["name"]+" (v"+self.popularNarratives[k]["version"]+")<br><i><small>modified on "+
-			self.monthLookup[d.getMonth()]+" "+d.getDate()+", "+d.getFullYear()+" at " + time +
-			"</small></i>";
-			
-			$tbl.append($("<tr>").append($("<td>").append(objhtml)));
-		    }
-		    self.$mainPanel.append($tbl);
-		} else {
-		    self.$mainPanel.append("<b>No visible narratives.</b>");
-		}
-	    }
-	    else {
-		self.$mainPanel.append('<div id="loading-mssg"><p class="muted loader-table"><center><img src="assets/img/ajax-loader.gif"><br><br>searching for recent activity...</center></p></div>');
-	    }
-	    
-	    
-	},
-        monthLookup : ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep","Oct", "Nov", "Dec"]
-    
+define(['jquery', 'nunjucks', 'kbasesocialwidget'], function ($, nunjucks, SocialWidget) {
+	var PopularNarrativesWidget = Object.create(SocialWidget, {
+		init: {
+			value: function (cfg) {
+				cfg.name = 'UserPopularNarratives';
+				cfg.title = 'Popular Narratives';
+				this.SocialWidget_init(cfg);
 
-    });
-})( jQuery )
+				this.templates.env.addFilter('dateFormat', function(dateString) {					
+					var d = new Date(dateString);
+					var shortMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+					var time = "";
+					var minutes = d.getMinutes();
+					if (minutes < 10) {
+						minutes = "0" + minutes;
+					}
+					if (d.getHours() >= 12) {
+						if (d.getHours() != 12) {
+							time = (d.getHours() - 12) + ":" + minutes + "pm";
+						} else {
+							time = "12:" + minutes + "pm";
+						}
+					} else {
+						time = d.getHours() + ":" + minutes + "am";
+					}
+					return shortMonths[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear() + " at " + time;
+                });
+                this.templates.env.addFilter('typeIcon', function (type) {
+                	var initial = type.charAt(0);
+                	return '<span style="border: 1px red solid; padding: 6px;font-weight: bold; font-size: 150%">' + initial + '</span>';
+                })
+				
+				if (cfg.workspaceURL) {
+					if (this.auth.authToken) {
+						this.workspaceClient = new Workspace(cfg.workspaceURL, {
+							token: this.auth.authToken
+						});
+					} else {
+						this.workspaceClient = new Workspace(cfg.workspaceURL); 
+					}
+				} else {
+					throw 'The workspace client url is not defined';
+				}
+
+				// TODO: get this from somewhere, allow user to configure this.
+				this.params.limit = 10;
+
+                return this;
+			}
+		},
+
+		render: {
+			value: function () {
+				//console.log('Context');
+				//console.log(this.context);
+				this.places.content.html(this.getTemplate('popular_narratives').render(this.context));
+			}
+		},
+
+		getCurrentState: {
+			value: function(cfg) {
+				// Reset or create the popular narratives list.
+				this.data.popularNarratives = [];
+
+
+				// Note that Narratives are now associated 1-1 with a workspace. 
+				// Some new narrative attributes, such as name and (maybe) description, are actually
+				// stored as attributes of the workspace itself.
+				// At present we can just use the presence of "narrative_nice_name" metadata attribute 
+				// to flag a compatible workspace.
+				var d = new Date();
+				d.setMonth(d.getMonth() - 3);
+				this.workspaceClient.list_workspace_info({					
+					showDeleted: 0,
+					owners: [this.params.userId]
+				}, function(data) {
+					var workspaceIds = [];
+					var workspaceMap = {};
+
+					// First we both transfor each ws info object into a nicer js object,
+					// and filter for modern narrative workspaces.
+					for (var i=0; i<data.length; i++) {
+						//tuple<ws_id id, ws_name workspace, username owner, timestamp moddate,
+						//int object, permission user_permission, permission globalread,
+						//lock_status lockstat, usermeta metadata> workspace_info
+						var wsInfo = {
+							id: data[i][0],
+							name: data[i][1],
+							owner: data[i][2],
+							modified: data[i][3],
+							object_count: data[i][4],
+							permission: data[i][5],
+							globalread: data[i][6],
+							lockstat: data[i][7],
+							metadata: data[i][8]
+						};
+
+						if (wsInfo.metadata.narrative_nice_name) {
+							workspaceIds.push(wsInfo.id);
+							workspaceMap[wsInfo.name] = wsInfo;
+						}
+					}
+
+					// Then we need to get the actual narratives because we need the ids in order
+					// to form a url.
+					// NB this is a bit awkward -- perhaps there will be a way soon to open a narrative
+					// just by specifying the workspace.
+
+					// Get details for, sort, and limit the list of workspace objects.
+					if (workspaceIds.length > 0) {
+						
+						var workspacesWithNarratives = {};
+						this.workspaceClient.list_objects({
+								savedby: [this.params.userId],
+								ids: workspaceIds,
+								type: 'KBaseNarrative.Narrative',
+								includeMetadata: 1
+							},
+							function(data) { 
+								for (var i=0; i<data.length; i++) {
+									//console.log('data: ' + i);
+									//console.log(data[i]);
+									//<obj_id objid, obj_name name, type_string type,
+									//timestamp save_date, int version, username saved_by,
+									//ws_id wsid, ws_name workspace, string chsum, int size, usermeta meta>
+									// only consider narratives.
+									// just get the second component of the type name.
+									// var dataType = (data[i][2].split("-")[0]).split("\.")[1];
+									//if (dataType === 'Narrative') {
+									var narrativeInfo = {
+										id: data[i][0],
+										name: data[i][1],
+										type: data[i][2],
+										save_date: data[i][3],
+										version: data[i][4],
+										saved_by: data[i][5],
+										wsid: data[i][6],
+										ws: data[i][7],
+										// type: dataType, 
+										checksum: data[i][8],
+										size: data[i][9],
+										meta: data[i][10],
+										ref: data[i][7] + '/' + data[i][1],
+										obj_id: 'ws.' + data[i][6] + '.obj.' + data[i][0]
+									};
+									var workspaceName = narrativeInfo.ws;
+									if (workspacesWithNarratives[workspaceName]) {
+										this.addWarningMessage('Workspace '+workspaceName+' already has a narrative ' +
+											workspacesWithNarratives[workspaceName].name + ', ' +
+											narrativeInfo.name + ' was skipped.');
+									} else {
+										
+										//var dataType = narrativeInfo.(data[i][2].split("-")[0]).split("\.")[1];
+										var workspaceInfo = workspaceMap[narrativeInfo.ws];
+										if (workspaceInfo) {
+											narrativeInfo.nice_name = workspaceInfo.metadata.narrative_nice_name;
+											narrativeInfo.description = workspaceInfo.metadata.narrative_description;
+											this.data.popularNarratives.push(narrativeInfo);
+										} else {
+											this.addWarningMessage('Workspace ' + narrativeInfo.ws + ' for narrative '+narrativeInfo.name + ' not found.');
+										}
+										
+										workspacesWithNarratives[workspaceName] = narrativeInfo;
+									}
+
+								}
+
+								// Sort by the version, from higher to lower.
+								this.data.popularNarratives.sort(function(a, b) {
+									if (a.version < b.version) {
+										return 1;
+									} else if (a.version > b.version) {
+										return -1;
+									} else {
+										return 0;
+									}
+								});
+								
+								if (this.data.popularNarratives.length > this.params.limit) {
+									this.data.popularNarratives = this.data.popularNarratives.slice(0, this.params.limit);
+								}
+
+								cfg.success();
+
+							}.bind(this),
+							function(err) {
+								cfg.error(err.error.message);
+							}.bind(this));
+					} else {
+						// Didn't find anything, but still considered "success"
+						cfg.success();
+					}
+				}.bind(this),
+				function(err) {
+					cfg.error(err.error.message);
+				}.bind(this));
+			}	
+		}
+
+	});
+
+	return PopularNarrativesWidget;
+});
