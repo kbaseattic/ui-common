757b0219698a6737d648e99fc143e0fefd9203f1
Jim Thomason <thomason@cshl.edu>
2015-01-21 09:58:08 -0800

    put info box back in

--- src/widgets/narrativemanager/kbaseNarrativeStoreView.js
+++ src/widgets/narrativemanager/kbaseNarrativeStoreView.js
@@ -42,10 +42,19 @@
             return this;
         },
 
+        getParameterByName : function(name) {
+          name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+          var regexS = "[\\?&]" + name + "=([^&#]*)";
+          var regex = new RegExp(regexS);
+          var results = regex.exec(window.location.search);
+          if(results == null)
+            return "";
+          else
+            return decodeURIComponent(results[1].replace(/\+/g, " "));
+        },
+
         getNarMethodStoreInfo: function() {
             var self = this;
-
-            return;
 
             this.narstore.status(
                     function(status) {
