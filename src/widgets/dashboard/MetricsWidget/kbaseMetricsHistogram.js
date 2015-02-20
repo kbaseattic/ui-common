b39d5f019231701738a32ad13d4cc0e5e5c4f88b
Roman Sutormin <rsutormin@lbl.gov>
2015-02-20 14:33:56 -0800

    Further improvement of d3 version of metrics histograms.

--- src/widgets/dashboard/MetricsWidget/kbaseMetricsHistogram.js
+++ src/widgets/dashboard/MetricsWidget/kbaseMetricsHistogram.js
@@ -3,6 +3,8 @@
         name: "kbaseMetricsHistogram", 
         version: "1.0.0",
         
+        tooltip: null,
+        
         options: {
             state: null,
             x_axis: null,
@@ -15,22 +17,41 @@
             var container = self.$elem;
             var id = container.attr('id');
             container.empty();
-            var w = 400;
             var h = 120;
             container.css({
                 'margin-left': '0px',
                 'margin-top': '0px', 
-                'height': '', 
-                'display': 'inline-block',
-                'position': 'relative',
-                'width': '100%',
-                'padding-bottom': '30%', /* aspect ratio */
-                'vertical-align': 'top',
-                'overflow': 'hidden'
+                'height': h+'px', 
+                'width': '100%'
             });
-            container.append('<svg id="vis_'+id+'" style="display: inline-block; position: absolute; top: 0px; left: 0"></svg>');
+            var w = container.width();
+            container.append('<svg id="vis_'+id+'" style="height: '+h+'px"></svg>');
+            self.tooltip = d3.select("body")
+            .append("div")
+            .classed("kbcb-tooltip", true);
+            self.render();
+            var onResize = null;
+            onResize = function() {
+                if (!container.is(':visible')) {
+                    $(window).off('resize', onResize);
+                } else {
+                    self.render();
+                }
+            };
+            $(window).on('resize', onResize);
+            return this;
+        },
+        
+        render: function() {
+            var self = this;
+            var container = self.$elem;
+            var id = container.attr('id');
+            var w = container.width();
+            var h = container.height();
             var charts = this.options.state.chart;
-            var vis = d3.select('#vis_'+id).attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 "+w+" "+h);
+            var vis = d3.select('#vis_'+id).style("width", w+'px');
+            vis.selectAll("*").remove();
+            
             var mar = {
               top: 25,
               right: 0,
@@ -51,7 +72,6 @@
                 if (userVal >= charts[i].lower && userVal < charts[i].upper)
                     binNum = i;
             }
-            console.log("binNum=" + binNum + ", userVal=" + userVal);
             var xDomain = barData.map(function (d) {
                 return d.x;
             });
@@ -76,16 +96,17 @@
             })
             .attr("text-anchor", "middle")
             .attr("font-size", "9px")
-            .attr("fill", "gray");
+            .attr("fill", "gray")
+            .attr("font-style", "italic");
             
             xAxis = d3.svg.axis()
             .scale(xRange)
-            .tickSize(3)
+            .tickSize(0)
             .tickSubdivide(true),
 
             yAxis = d3.svg.axis()
             .scale(yRange)
-            .tickSize(3)
+            .tickSize(0)
             .orient("left")
             .tickSubdivide(true);
             
@@ -109,13 +130,15 @@
                 return Math.max(1, (h - mar.bottom) - yRange(d.y) - .1);
             })
             .attr('fill', '#CECECE')
-            .on('mouseover',function(d){
+            .on('mouseover',function(d) {
                 d3.select(this)
-                .attr('fill','blue');
+                .attr('fill','gray');
+                self.showTooltip(true, vis, this, xRange(d.x) + xRange.rangeBand() + 3, yRange(d.y) - 10, "" + d.y);
             })
             .on('mouseout',function(d){
                 d3.select(this)
                 .attr('fill','#CECECE');
+                self.showTooltip(false);
             });
             
             vis.append('svg:g')
@@ -144,6 +167,9 @@
             if (binNum) {
                 var bin = barData[binNum];
                 userX = xRange(bin.x) + (userVal - bin.lower + 0.5) * xRange.rangeBand() / bin.size - 2;
+            } else if (barData.length > 0) {
+                var bin = barData[0];
+                userX = xRange(bin.x) - 5;
             }
             var flagY = yRange(yMax) - 15;
 
@@ -155,7 +181,12 @@
             .attr('fill', 'rgba(75,184,86,0.7)');
 
             vis.append('polygon')
-            .attr('points', "" + (userX + 5) + "," + flagY + ", " + (userX + 30) + "," + (flagY + 7) + ", " + (userX + 5) + "," + (flagY + 14))
+            .attr('points', "" + 
+                    (userX + 5) + "," + flagY + ", " + 
+                    (userX + 30) + "," + flagY + ", " + 
+                    (userX + 17) + "," + (flagY + 7) + ", " + 
+                    (userX + 30) + "," + (flagY + 14) + ", " + 
+                    (userX + 5) + "," + (flagY + 14))
             .attr('fill', 'rgba(75,184,86,0.7)');
 
             vis.append('text')
@@ -165,16 +196,35 @@
             .attr('fill', 'gray')
             .attr("font-size", "9px");
             
-            /*var onResize = null;
-            onResize = function() {
-                var newW = container.width();
-                var newH = container.height();
-                if (!container.is(':visible'))
-                    $(window).off('resize', onResize);
-                vis.attr('width', newW).attr('height', newH);
-            };
-            $(window).on('resize', onResize);*/
             return this;
+        },
+        
+        showTooltip: function(show, vis, thisObj, x, y, value) {
+            var self = this;
+            if (!show) {
+                self.tooltip.style("visibility", "hidden");
+                return;
+            }
+            var e = d3.mouse(thisObj);
+            var targetel = thisObj; // d3.select(this);
+            while ('undefined' === typeof targetel.getScreenCTM && 'undefined' === targetel.parentNode) {
+                targetel = targetel.parentNode;
+            }
+            var el = vis.node();
+            if (el.tagName.toLowerCase() !== 'svg') {
+                el = el.ownerSVGElement;
+            }
+            var point = el.createSVGPoint();
+            point.x = x ? x : e[0];
+            point.y = y ? y : e[1];
+            var matrix = targetel.getScreenCTM();
+            var absPoint = point.matrixTransform(matrix);
+            var scrollTop  = document.documentElement.scrollTop || document.body.scrollTop;
+            var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+            self.tooltip.text(value);
+            var tooltipH = $(self.tooltip[0]).height();
+            self.tooltip.style("top", (absPoint.y + scrollTop - tooltipH - 2) + "px").style("left", (absPoint.x + scrollLeft + 2) + "px");
+            self.tooltip.style("visibility", "visible");
         },
         
         uuid: function() {
