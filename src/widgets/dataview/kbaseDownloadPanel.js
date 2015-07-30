c0123dcc75e69dd58b502239c6227ce37aaa46cb
Erik Pearson <eapearson@lbl.gov>
2015-07-30 15:51:42 -0700

    replace the shock config usage with service.shock.url

--- src/widgets/dataview/kbaseDownloadPanel.js
+++ src/widgets/dataview/kbaseDownloadPanel.js
@@ -125,7 +125,7 @@ define(['jquery', 'kb.jquery.widget', 'kb.runtime', 'kb.service.workspace', 'kb.
             this.wsUrl = R.getConfig('service.workspace.url');
             this.transformURL = R.getConfig('transform');
             this.ujsURL = R.getConfig('service.user_job_state.url');
-            this.shockURL = R.getConfig('shock');
+            this.shockURL = R.getConfig('service.shock.url');
             this.exportURL = R.getConfig('service.data_import_export.url');
             this.token = R.getAuthToken();
             this.type = null; //this.options.type;
