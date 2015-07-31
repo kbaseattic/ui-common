06a2f34d2a45bfd94a4d4a91f600759e7c793564
Bill Riehl <wjriehl@lbl.gov>
2015-07-30 20:44:14 -0700

    Added a couple more trimmed down test specs (to be fleshed out).

---
+++ test/spec/kbaseAppSpec.js
@@ -0,0 +1,11 @@
+'use strict';
+define(['kb.config'], function(Config) {
+
+    describe('The Config does things!', function() {
+
+        it('Should get a config', function() {
+            expect(Config).toBeTruthy();
+        });
+    });
+
+});
