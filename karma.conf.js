7355a7d88ee17eb2f72b8c45909d33493fe02fa3
Bill Riehl <wjriehl@lbl.gov>
2015-07-30 21:27:32 -0700

    Got a coverage report (Istanbul, via the karma-js-coverage plugin) working.

--- karma.conf.js
+++ karma.conf.js
@@ -82,13 +82,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'src/**/!(datavis|Tiling_widget|postal.request-response.q).js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
