ce2fb2ca6ea3a6beff1fd313fb4c5affd1e82c7e
Bill Riehl <wjriehl@lbl.gov>
2015-08-03 14:06:41 -0700

    Added copy config step to gruntfile - will update build with it later.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,17 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-filerev');
     grunt.loadNpmTasks('grunt-regex-replace');
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        'copy': {
+            'build-config': {
+                src: 'source/config/prod.yml',
+                dest: 'build/config.yml',
+            },
+        },
 
         // Compile the requirejs stuff into a single, uglified file.
         // the options below are taken verbatim from a standard build.js file
@@ -85,6 +93,10 @@ module.exports = function(grunt) {
         'requirejs',
         'filerev',
         'regex-replace'
+    ]);
+
+    grunt.registerTask('build-config', [
+        'copy'
     ]);
 
     grunt.registerTask('test', [
