cf2d8b1e19ce70fb2db7ee9f327bed0af82b7f48
Bill Riehl <wjriehl@lbl.gov>
2015-10-02 11:17:35 -0700

    Tweak to fix report to coveralls

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -95,7 +95,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/**/!(datavis|Tiling_widget|postal.request-response.q).js': ['coverage']
+      // 'build/client/**/!(datavis|Tiling_widget|postal.request-response.q).js': ['coverage']
+      'build/client/!(bower_components)/**/*.js': ['coverage']
     },
 
     coverageReporter: {
