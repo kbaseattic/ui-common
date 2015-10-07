3a14d9553b85cb80adea3f532058336ac3db0aab
Bill Riehl <wjriehl@lbl.gov>
2015-10-07 15:06:13 -0700

    moved output of config builder to build/client/config.yml

--- tools/process_config.js
+++ tools/process_config.js
@@ -2,18 +2,20 @@
 var iniParser = require('node-ini');
 var fs = require('fs');
 var _ = require('lodash');
-var outFile = 'build/config.yml'
+
+var serviceTemplateFile = 'config/service-config-template.yml';
+var settingsConfig = 'config/settings.yml';
+var outFile = 'build/client/config.yml';
 
 // grabs the deploy config file, by default deploy.cfg
 var deployCfg = iniParser.parseSync('deploy.cfg');
-fs.readFile('config/service-config-template.yml', 'utf8', function(err, serviceTemplate) {
+fs.readFile(serviceTemplateFile, 'utf8', function(err, serviceTemplate) {
     if (err) return console.log(err);
 
     var compiled = _.template(serviceTemplate);
     var services = compiled(deployCfg['ui-common']);
-    console.log(services);
 
-    fs.readFile('config/settings.yml', 'utf8', function(err, settings) {
+    fs.readFile(settingsConfig, 'utf8', function(err, settings) {
         if (err) return console.log(err);
 
         fs.writeFile(outFile, services + '\n\n' + settings, function(err) {
