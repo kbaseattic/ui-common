7a3604d0d2b62b256d78d6bc59bd47c5c5ade6b9
Jim Thomason <jim@japanesewartuba.com>
2016-02-12 21:10:30 -0600

    fixed color and scale alignment error

--- src/widgets/kbaseExpressionMatrixHeatmap.js
+++ src/widgets/kbaseExpressionMatrixHeatmap.js
@@ -29,7 +29,6 @@ define('kbaseExpressionMatrixHeatmap',
 
         setDataset : function setDataset(newDataset) {
             this.data('loader').hide();
-            this.data('heatElem').show();
 
             if (newDataset.data.values.length == 0) {
                 this.$elem.empty();
@@ -38,8 +37,24 @@ define('kbaseExpressionMatrixHeatmap',
                     .html("Could not load object : " + newDataset.description);
             }
             else {
+
+                var $heatElem = $.jqElem('div').css({width : 800, height : newDataset.data.row_ids.length * 10});
+
+                var $heatmap =
+                    $heatElem.kbaseHeatmap(
+                        {
+                            xPadding : 170,
+                        }
+                    )
+                ;
+                this.data('heatmap', $heatmap);
+                this.data('heatElem', $heatElem);
+
+                this.$elem.append($heatElem);
+
+
                 //newDataset.data = newDataset.values;
-                this.data('heatElem').css('height', newDataset.data.row_ids.length * 10);
+                this.data('heatElem')
                 this.data('heatmap').setDataset(
                     {
                         row_ids : newDataset.data.row_ids,
@@ -88,13 +103,8 @@ define('kbaseExpressionMatrixHeatmap',
 
             var $me = this;
 
-            var $heatElem = $.jqElem('div').css({width : 800, height : 500}).css('display', 'none')
-                .attr('id', 'heatElem')
-            ;
-
 
             $elem
-                .append( $heatElem )
                 .append(
                     $.jqElem('div')
                         .attr('id', 'loader')
@@ -108,23 +118,9 @@ define('kbaseExpressionMatrixHeatmap',
                 )
             ;
 
-            //XXX - I don't want to fix this right now. Setting yPadding to 0 will keep the background of the yPadding from overlaying
-            //on the chart, but it'll cut off half of the label at the bottom of the temp gauge.
-            //
-            //kbaseFigureObjectHeatmap has a better method for this, moving the hmElem declartion into setDataset. Easy to do,
-            //I just don't want to test it ATM.
-
-            var $heatmap =
-                $heatElem.kbaseHeatmap(
-                    {
-                        yPadding : 0
-                    }
-                )
-            ;
 
 
             this._rewireIds($elem, this);
-            this.data('heatmap', $heatmap);
 
         },
 
